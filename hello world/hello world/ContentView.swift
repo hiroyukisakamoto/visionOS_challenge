f6da339b332bf167bc180c4729e0577c6cdc5241
hiroyukisakamoto <h.sakamoto.design@gmail.com>
2024-07-09 18:16:15 +0900

    add : hello world , Rotate 3D Obj

---
+++ hello world/hello world/ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("hello_worldApp")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.clear) 
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello world/hello world/hello_worldApp.swift
@@ -0,0 +1,19 @@
+//
+//  hello_worldApp.swift
+//  hello world
+//
+//  Created by jetz on 2024/07/09.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup() {
+            ContentView()
+                .glassBackgroundEffect()
+                .frame(width: 1280, height: 720)
+        }
+    }
+}
